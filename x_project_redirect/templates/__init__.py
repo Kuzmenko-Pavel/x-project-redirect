467356368111ec8c7a3314c97554c98bc56cf229
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-02-13 11:40:55 +0200

    fix cid

--- x_project_redirect/templates/__init__.py
+++ x_project_redirect/templates/__init__.py
@@ -1,7 +1,15 @@
 import aiohttp_jinja2
 import jinja2
 
+from x_project_redirect.templates.jinja2htmlcompress import HTMLCompress
+
 
 def init_templates(app):
     aiohttp_jinja2.setup(
-        app, loader=jinja2.PackageLoader('x_project_redirect', 'templates'))
+        app,
+        loader=jinja2.PackageLoader('x_project_redirect', 'templates'),
+        trim_blocks=True,
+        lstrip_blocks=True,
+        enable_async=True,
+        extensions=[HTMLCompress]
+    )
